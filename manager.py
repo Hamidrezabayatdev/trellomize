58adce841c2e9a03b957ee3a085d6d2b72c5b2cd
PouriaGolsorkhi <152257136+PouriaGolsorkhi@users.noreply.github.com>
2024-05-22 22:19:01 +0330

    managerPy completed (hopefully)

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 def createAdmin(name, password):
     Manager = {
         "name": name,
@@ -7,11 +8,13 @@ def createAdmin(name, password):
     }
     p=True
     with open("managerInfo.json", 'r') as exFile:
-        if exFile["name"] !="" :
+        if os.path.getsize("managerInfo.json") !=0 :
             p=False
     if p:
         with open("managerInfo.json", 'w') as maFile:
             json.dump(Manager, maFile)
+    else:
+        print("A manager already exists! you can't create a new one.")
 
      
 parser = argparse.ArgumentParser()
