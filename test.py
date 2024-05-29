3215e378188396c39e79eb1900518fb1949f6985
PouriaGolsorkhi <152257136+PouriaGolsorkhi@users.noreply.github.com>
2024-05-30 02:13:46 +0330

    logger is being added to test.py

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import unittest
 import main
+import logging
 
+#logging :
+logger3 = logging.getLogger("test.py")
+
+logging.basicConfig(filename="user_actions.log",
+                    filemode='a',
+                    format='%(asctime)s, %(name)s %(levelname)s %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    level=logging.INFO)
 class TestMyProgram(unittest.TestCase):
     def test_getHashedPassfunc(self):
         passExample="testpassword"
