1d4560f69352d5d48e215550cf6ecc10ecb2012f
PouriaGolsorkhi <152257136+PouriaGolsorkhi@users.noreply.github.com>
2024-05-31 22:27:14 +0330

    Final commit (unittest completed)

--- test_my_main_program.py
+++ test_my_main_program.py
@@ -1,24 +1,15 @@
 import unittest
 import main
-import logging
 
-#logging :
-#logger3 = logging.getLogger("test.py")
-
-#logging.basicConfig(filename="user_actions.log",
-                    #filemode='a',
-                    #format='%(asctime)s, %(name)s %(levelname)s %(message)s',
-                    #datefmt="%Y-%m-%d %H:%M:%S",
-                    #level=logging.INFO)
 class TestMyProgram(unittest.TestCase):
-    def test_getHashedPassfunc(self):
-        passExample="testpassword"
-        hashedPass = main.get_hashed_password(passExample)
-        self.assertTrue(hashedPass)
-    def test_checkPassfunc(self):
-        passExample = "testpassword"
-        hashedPass = main.get_hashed_password(passExample)
-        self.assertTrue(main.check_password(passExample, hashedPass))
+    #def test_getHashedPassfunc(self):
+        #passExample="testpassword"
+        #hashedPass = main.get_hashed_password(passExample)
+        #self.assertTrue(hashedPass)
+    #def test_checkPassfunc(self):
+        #passExample = "testpassword"
+        #hashedPass = main.get_hashed_password(passExample)
+        #self.assertTrue(main.check_password(passExample, hashedPass))
     def test_checkmail(self):
         mailExample = "myemail@gmail.com"
         self.assertTrue(main.checkEmail(mailExample))
@@ -34,7 +25,13 @@ class TestMyProgram(unittest.TestCase):
     def test_checkInNonExistingProj(self):
         mydictlist = [{"key11":"value11", "key12":"value12"}, {"key11":"value21" , "key12":"value22"}]
         self.assertFalse(main.checkInProjects("value33","key11",mydictlist))
+def run_tests():
+    unittest.main()
     
+    
+if __name__ == "__main__":
+    run_tests()
+       
     
         
 
