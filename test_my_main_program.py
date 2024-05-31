bc427c4fcd740c04f1f0cab5e20c9d0b7cb9476a
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2024-05-31 22:31:57 +0330

    final commit 2

--- test_my_main_program.py
+++ test_my_main_program.py
@@ -2,14 +2,14 @@ import unittest
 import main
 
 class TestMyProgram(unittest.TestCase):
-    #def test_getHashedPassfunc(self):
-        #passExample="testpassword"
-        #hashedPass = main.get_hashed_password(passExample)
-        #self.assertTrue(hashedPass)
-    #def test_checkPassfunc(self):
-        #passExample = "testpassword"
-        #hashedPass = main.get_hashed_password(passExample)
-        #self.assertTrue(main.check_password(passExample, hashedPass))
+    def test_getHashedPassfunc(self):
+        passExample="testpassword"
+        hashedPass = main.get_hashed_password(passExample)
+        self.assertTrue(hashedPass)
+    def test_checkPassfunc(self):
+        passExample = "testpassword"
+        hashedPass = main.get_hashed_password(passExample)
+        self.assertTrue(main.check_password(passExample, hashedPass))
     def test_checkmail(self):
         mailExample = "myemail@gmail.com"
         self.assertTrue(main.checkEmail(mailExample))
